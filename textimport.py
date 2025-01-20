e627524d42098cd8721f348304e0e6439a1007e1
Hugo L. <hugoleon.ie@gmail.com>
2025-01-20 15:56:47 +0100

    removing unneeded lines

--- textimport.py
+++ textimport.py
@@ -3,13 +3,10 @@
 import pdftotext
 import sys
 
-#x = input('File to read: ')
 x = sys.argv[1]
 
 with open(x, "rb") as f:
     pdf = pdftotext.PDF(f)
-
-#print(len(pdf))
 
 for page in pdf:
     print(page)
